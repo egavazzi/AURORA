952e83b456818bf04555479b0e0320e1f73a476f
Etienne <etienne.gavazzi@uit.no>
2022-05-03 12:18:53 +0000

    clearing up the code a bit

--- MI_coupling/conversion.m
+++ MI_coupling/conversion.m
@@ -32,29 +32,24 @@ HMR_MU = 20;   % HOW MUCH DO YOU WANT TO REFINE MU
 F = griddedInterpolant(1:101,vz_grid);
 vz_grid_finer = F(1:(1/HMR_VZ):101);
 dvz_finer = (1/HMR_VZ) * dvz ;
-% vz_middle_bin_finer = vz_middle_bin;
 vz_middle_bin_finer = vz_grid_finer(2:end) - 0.5 * dvz_finer;
 % Refine mu_mag-grid
 G = griddedInterpolant(1:101,mu_mag_grid);
 mu_mag_grid_finer = G(1:(1/HMR_MU):101);
 dmu_mag_finer = (1/HMR_MU) .* dmu_mag;
 dmu_mag_finer = repelem(dmu_mag_finer,HMR_MU);
-% mu_mag_middle_bin_finer = mu_mag_middle_bin;
 mu_mag_middle_bin_finer = mu_mag_grid_finer(2:end) - 0.5 .* dmu_mag_finer;
 % Refine f
 fzvzmu_in_finer = repelem(fzvzmu_in,HMR_VZ,HMR_MU);
 
 % Make E-grid
 dEfcn = @(X,DEinitial,DEfinal,C,X0) DEinitial+(1+tanh(C*(X-X0)))/2*DEfinal;     % from setup4etrpd10streams.m
-E_grid = cumsum(dEfcn(0:1600,0.15,11.5,0.05,80))+1.9;
-E_grid = E_grid;
-dE = diff(E_grid);
-E_middle_bin = E_grid(1:(end-1)) + 0.5 * dE;
+E_grid = cumsum(dEfcn(0:1000,0.15,11.5,0.05,80))+1.9;
+E_middle_bin = E_grid;
+dE = diff(E_grid);dE = [dE,dE(end)];
 % Make mu_pitch-grid
-% theta_lims2do = [180 170 150 120 100 90];
-% % theta_lims2do = [180 170 150 130 127.5 125 122.5 120 117.5 115 110 105 100 95 92.5 90];
 theta_lims2do = [180:-5:90];
-mu_pitch_grid = abs(cosd(theta_lims2do));
+mu_pitch_grid = cosd(theta_lims2do);
 BeamW = [];
 for iMu = (numel(mu_pitch_grid)-1):-1:1
   BeamW(iMu) = 2*pi * abs(integral(@(pa) sin(pa),acos(mu_pitch_grid(iMu)),acos(mu_pitch_grid(iMu+1))));
@@ -63,8 +58,6 @@ mu_pitch_middle_bin = mu_avg(mu_pitch_grid);
 
 
 Ie_1 = zeros(numel(E_middle_bin),numel(mu_pitch_middle_bin));
-
-% v = zeros(length(vz_middle_bin_finer),length(mu_mag_middle_bin_finer));
 
 v = zeros(length(vz_middle_bin),length(mu_mag_middle_bin));
 for ii = 1:length(vz_middle_bin)
@@ -78,18 +71,14 @@ for ii = 1:length(vz_middle_bin_finer)
   for jj = 1:length(mu_mag_middle_bin_finer)
     % get coordinate point in (E,mu_pitch)-grid
     E = 0.5 * m * (vz_middle_bin_finer(ii).^2 + 2*B/m .* mu_mag_middle_bin_finer(jj)) ./ 1.6e-19;
-    mu_pitch = cos(atan(sqrt(2*B/m * mu_mag_middle_bin_finer(jj)) / vz_middle_bin_finer (ii)));
-%     v(ii,jj) = sqrt(vz_middle_bin_finer(ii).^2 + 2*B/m .* mu_mag_middle_bin_finer(jj));
+    mu_pitch = - cos(atan(sqrt(2*B/m * mu_mag_middle_bin_finer(jj)) / vz_middle_bin_finer (ii)));
     [~,index_pitch] = min(abs(mu_pitch_middle_bin - mu_pitch));
     [~,index_energy] = min(abs(E_middle_bin - E));
     % convert distribution function and compute flux (/eV/ster)
     Ie_1(index_energy,index_pitch) = Ie_1(index_energy,index_pitch) + ...
                                 v(ii,jj) * fzvzmu_in_finer(ii,jj) * ...
-                                ... 1 * fzvzmu_in_finer(ii,jj) * ...
                                 dvz_finer * dmu_mag_finer(jj) ./ ...
                                 dE(index_energy);
-                                %(BeamW(index_pitch) ./ sum(BeamW));
-                                %(1 * dE(index_energy) * BeamW(index_pitch));
   end
 end
 Ie_total_1 = sum(((v .* fzvzmu_in_finer) * dmu_mag_finer.') * dvz_finer)     % [#e/m2/s]
@@ -119,7 +108,6 @@ HMR_MU = 20;   % HOW MUCH DO YOU WANT TO REFINE MU
 F = griddedInterpolant(1:101,vz_grid);
 vz_grid_finer = F(1:(1/HMR_VZ):101);
 dvz_finer = (1/HMR_VZ) * dvz ;
-% vz_middle_bin_finer = vz_middle_bin;
 vz_middle_bin_finer = vz_grid_finer(2:end) - 0.5 * dvz_finer;
 % Refine mu_mag-grid
 G = griddedInterpolant(1:101,mu_mag_grid);
@@ -133,15 +121,12 @@ fzvzmu_in_finer = repelem(fzvzmu_in,HMR_VZ,HMR_MU);
 
 % Make E-grid
 dEfcn = @(X,DEinitial,DEfinal,C,X0) DEinitial+(1+tanh(C*(X-X0)))/2*DEfinal;     % from setup4etrpd10streams.m
-E_grid = cumsum(dEfcn(0:1600,0.15,11.5,0.05,80))+1.9;
-E_grid = E_grid;
-dE = diff(E_grid);
-E_middle_bin = E_grid(1:(end-1)) + 0.5 * dE;
+E_grid = cumsum(dEfcn(0:1000,0.15,11.5,0.05,80))+1.9;
+E_middle_bin = E_grid;
+dE = diff(E_grid);dE = [dE,dE(end)];
 % Make mu_pitch-grid
-% theta_lims2do = [180 170 150 120 100 90];
-% % theta_lims2do = [180 170 150 130 127.5 125 122.5 120 117.5 115 110 105 100 95 92.5 90];
 theta_lims2do = [180:-5:90];
-mu_pitch_grid = abs(cosd(theta_lims2do));
+mu_pitch_grid = cosd(theta_lims2do);
 BeamW = [];
 for iMu = (numel(mu_pitch_grid)-1):-1:1
   BeamW(iMu) = 2*pi * abs(integral(@(pa) sin(pa),acos(mu_pitch_grid(iMu)),acos(mu_pitch_grid(iMu+1))));
@@ -162,27 +147,27 @@ for ii = 1:length(vz_middle_bin_finer)
   for jj = 1:length(mu_mag_middle_bin_finer)
     % get coordinate point in (E,mu_pitch)-grid
     E = 0.5 * m * (vz_middle_bin_finer(ii).^2 + 2*B/m .* mu_mag_middle_bin_finer(jj)) ./ 1.6e-19;
-    mu_pitch = cos(atan(sqrt(2*B/m * mu_mag_middle_bin_finer(jj)) / vz_middle_bin_finer (ii)));
-%     v(ii,jj) = sqrt(vz_middle_bin_finer(ii).^2 + 2*B/m .* mu_mag_middle_bin_finer(jj));
+    mu_pitch = - cos(atan(sqrt(2*B/m * mu_mag_middle_bin_finer(jj)) / vz_middle_bin_finer (ii)));
     [~,index_pitch] = min(abs(mu_pitch_middle_bin - mu_pitch));
     [~,index_energy] = min(abs(E_middle_bin - E));
     % convert distribution function and compute flux (/eV/ster)
     Ie_3(index_energy,index_pitch) = Ie_3(index_energy,index_pitch) + ...
                                 v(ii,jj) * fzvzmu_in_finer(ii,jj) * ...
-                                ... 1 * fzvzmu_in_finer(ii,jj) * ...
                                 dvz_finer * dmu_mag_finer(jj) / ...
                                 dE(index_energy);
-                                %(BeamW(index_pitch) ./ sum(BeamW));
-                                %(1 * dE(index_energy) * BeamW(index_pitch));
   end
 end
 Ie_total_3 = sum(((v .* fzvzmu_in_finer) * dmu_mag_finer.') * dvz_finer)     % [#e/m2/s]
-E_total_3 = sum(E_middle_bin * (Ie_3 .* dE.'))                                 % [eV/m2/s]
+E_total_3 = sum(E_middle_bin * (Ie_3 .* dE.'))                               % [eV/m2/s]
 toc
+%% In which bins is the bulk of the energy flux
+E1 = E_middle_bin.' .* (Ie_1 .* dE.');
+R = sum(sum(E1(log10(E1)>1)))./sum(sum(E1)) % log10(E)>1 gives around 100% of the energy. Means we can reduce size of E-grid.
+
 %% plot Ie
-Ie_plot = Ie_1;
+% Ie_plot = Ie_1;
 % Ie_plot = Ie_3;
-% Ie_plot = Ie_1 + Ie_3; 
+Ie_plot = E1;%Ie_1 + Ie_3; 
 
 figure()
 c1 = jet(64); c2 = jet(256); c3 = jet(1024);
@@ -190,10 +175,10 @@ c = [c1(1:8,:);c2(32:64,:);c3(257:1024,:)];
 colormap(c)
 pp = log10(Ie_plot);
 ss = size(pp);
-pp = [[pp zeros(ss(1),1)];zeros(1,ss(2)+1)];
+pp = [pp zeros(ss(1),1)];
 Eplot = E_grid;
 muplot = acosd(mu_pitch_grid(1:end));
-h = polarPcolor(Eplot,(muplot),pp,'Ncircles',10);
+h = polarPcolor(Eplot,muplot,pp,'Ncircles',10,'Nspokes',10);
 
 % h = pcolor(Eplot,(muplot),pp.');
 % set(h,'EdgeColor','none');
@@ -205,7 +190,8 @@ h = polarPcolor(Eplot,(muplot),pp,'Ncircles',10);
 % cb = colorbar;
 % cb.Title.String = "log_{10}Ie (#e/m2/s/eV/ster)";
 
-caxis([-10 1])
+% caxis([-10 1])
+caxis([0 6])
 %% Write Ie_total coming at top of the ionosphere in a file to be used by AURORA
 clear Ie_total;
 for i_mu = 1:18   % downward fluxes
@@ -216,31 +202,38 @@ for i_mu = 19:36  % upward fluxes
 end
 save('incoming_flux.mat','-v7.3','Ie_total')
 
+
+%%
+%%
+%%
+%%
+%%
+load I_top.mat
+E_grid = E;
+
 %% Convert in the other way
 tic
 % Extrapolate Ie over a finer (E,mu_pitch) grid;
 HMR_E = 1;   % HOW MUCH DO YOU WANT TO REFINE E
 HMR_MU = 10;   % HOW MUCH DO YOU WANT TO REFINE MU
 % Refine E-grid
-F = griddedInterpolant(1:1801,E_grid);
-E_grid_finer = F(1:(1/HMR_E):2001);
+F = griddedInterpolant(1:1601,E_grid);
+E_grid_finer = F(1:(1/HMR_E):1601);
 dE_finer = (1/HMR_E) .* dE;
-dE_finer = repelem(dE_finer,HMR_E);
-E_middle_bin_finer = E_grid_finer(2:end) - 0.5 .* dE_finer;
+dE_finer = repelem(dE_finer,HMR_E); % or dE_finer = diff(E_grid_finer);dE_finer = [dE_finer,dE_finer(end)];
+E_middle_bin_finer = E_grid_finer;
 % Refine mu_pitch-grid
+theta_lims2do = 0:5:90;
 G = griddedInterpolant(1:length(theta_lims2do),theta_lims2do);  
 theta_lims2do_finer = G(1:(1/HMR_MU):length(theta_lims2do));
-
-mu_grid_finer = abs(cos(theta_lims2do_finer*pi/180));
+mu_grid_finer = cos(theta_lims2do_finer*pi/180);
 BeamW_finer = [];
 for iMu = (numel(mu_grid_finer)-1):-1:1
   BeamW_finer(iMu) = 2*pi * abs(integral(@(pa) sin(pa),acos(mu_grid_finer(iMu)),acos(mu_grid_finer(iMu+1))));
 end
 mu_pitch_middle_bin_finer = mu_avg(mu_grid_finer);
 
-% % Refine I
-% Ie_1_finer = Ie_1;
-% Ie_1_finer = repelem(Ie_1_finer./(HMR_E*HMR_MU),HMR_E,HMR_MU);
+% Refine I
 Ie_1_finer = Ie_1./ (BeamW ./ sum(BeamW));
 Ie_1_finer = repelem(Ie_1_finer./(HMR_E),HMR_E,HMR_MU);
 Ie_1_finer = Ie_1_finer .* (BeamW_finer ./ sum(BeamW_finer));
@@ -249,7 +242,6 @@ Ie_1_finer = Ie_1_finer .* (BeamW_finer ./ sum(BeamW_finer));
 f = zeros(numel(vz_middle_bin),numel(mu_mag_middle_bin));
 
 v = zeros(numel(vz_middle_bin),numel(mu_mag_middle_bin));
-% v = zeros(numel(E_middle_bin_finer),numel(mu_pitch_middle_bin_finer));
 for ii = 1:length(E_middle_bin_finer)
   for jj = 1:length(mu_pitch_middle_bin_finer)
     % get coordinate point in (vz,mu_mag)-grid
@@ -257,26 +249,23 @@ for ii = 1:length(E_middle_bin_finer)
     mu_mag = E_middle_bin_finer(ii) * 1.6e-19 ./ B * (1 - mu_pitch_middle_bin_finer(jj).^2);
     [~,index_vz] = min(abs(vz_middle_bin - vz));
     [~,index_mu_mag] = min(abs(mu_mag_middle_bin - mu_mag));
-%     v(ii,jj) = sqrt(2 * E_middle_bin_finer(ii) * 1.6e-19 / m);
     v(index_vz,index_mu_mag) = sqrt(vz_middle_bin(index_vz).^2 + 2*B/m .* mu_mag_middle_bin(index_mu_mag));
     % convert flux and compute distribution function
     f(index_vz,index_mu_mag) = f(index_vz,index_mu_mag) + 1 ./ ...
-                               ... v(ii,jj) * ...
                                v(index_vz,index_mu_mag) * ...  
                                Ie_1_finer(ii,jj) ./ ...
                                (dvz * dmu_mag(index_mu_mag)) * ...
                                dE_finer(ii);
-                               %dE_finer(ii) * BeamW_finer(jj) ./ ...
   end
 end
 Ie_total = sum((v .* f * dmu_mag.') * dvz)
 
 toc
 %% Plot f(vz,mu_mag)
-DIFF = log10(abs(fzvzmu_in .* (log10(fzvzmu_in) > -10) - f .* (log10(f) > -10)) ./ (fzvzmu_in .* (log10(fzvzmu_in) > -10)));
+% DIFF = log10(abs(fzvzmu_in .* (log10(fzvzmu_in) > -10) - f .* (log10(f) > -10)) ./ (fzvzmu_in .* (log10(fzvzmu_in) > -10)));
 % DIFF = - (log10(fzvzmu_in) .* (log10(fzvzmu_in) > -10) - log10(f).* (log10(f) > -10));
 
-ii = 3;
+ii = 1;
 zz = 300;
 
 figure
@@ -284,18 +273,18 @@ c1 = jet(64);c2 = jet(256);c3 = jet(1024);
 c = [c1(1:8,:);c2(32:64,:);c3(257:1024,:)];
 colormap(c)
 
-% pp = log10(fzvzmustruct((ii)).f(:,:,zz));
+pp = log10(fzvzmustruct((ii)).f(:,:,zz));
 % pp=log10(f(:,:));
-pp=(DIFF(:,:));
+% pp=(DIFF(:,:));
 
 ss = size(pp);
 pp = [[pp zeros(ss(1),1)] ; zeros(1,ss(2)+1)];
 vzplot = particle(ii).vzcorn;
 muplot = particle(ii).mucorn;
 
-h = pcolor(muplot,vzplot(101:end),pp);
+% h = pcolor(muplot,vzplot(101:end),pp);
 % h = pcolor(muplot,vzplot(101:end),pp(101:end,:));
-% h = pcolor(muplot,vzplot,pp);
+h = pcolor(muplot,vzplot,pp);
 
 axis([min(particle(ii).mucorn) ...
       max(particle(ii).mucorn) ...
